e256655b294bfc89f44c2b15f4b53ae51a5c5375
Maymoonah  <may.aloulah@gmail.com>
2023-06-18 20:42:30 +0300

    index new ver

--- index.php
+++ index.php
@@ -1,26 +1,6 @@
 <?php 
 // connection to db 
-
-$DB_HOST = "rm-l4v09mz21q0tr948qeo.mysql.me-central-1.rds.aliyuncs.com";
-    $DB_USER = "db_protestacc";
-    $DB_PASSWORD = "Amalshamrani1010$$";
-    $DB_DATABASE = "db_protest";
-	
-	
-	
-
-	//Connect to mysql server
-	$con = mysql_connect($DB_HOST, $DB_USER, $DB_PASSWORD);
-	if(!$con) {
-		die('Failed to connect to server: ' . mysql_error());
-	}
-	
-	//Select database
-	$db = mysql_select_db($DB_DATABASE);
-	mysql_query("SET CHARACTER SET 'utf-8'", $con); 
-	if(!$db) {
-		die("Unable to select database");
-	}
+include 'con.php'; 
 
 ?>
 
@@ -49,12 +29,22 @@ $DB_HOST = "rm-l4v09mz21q0tr948qeo.mysql.me-central-1.rds.aliyuncs.com";
 </head>
 <body>
 	
-	
-	<div class="bg-img1 size1 overlay1" style="background-image: url('images/bg01.jpg');">
+
+<div class="bg-img1 size1 overlay1" style="background-image: url('images/bg01.jpg');">
 		<div class="size1 p-l-15 p-r-15 p-t-30 p-b-50">
 			<div class="flex-w flex-sb-m p-l-75 p-r-60 p-b-165 respon1">
 				<div class="wrappic1 m-r-30 m-t-10 m-b-10">
-					
+					<div class="flex-w m-t-10 m-b-10">
+						<a href="mailto:abc@abc.com" class="size4 flex-c-m how-social trans-04 m-r-5 m-b-3 m-t-3">
+							e<i class="fa fa-email"></i>
+						</a>
+	
+						<a href="https://twitter.com/" class="size4 flex-c-m how-social trans-04 m-r-5 m-b-3 m-t-3">
+							<i class="fa fa-twitter"></i>
+						</a>
+	
+						
+					</div>
 				</div>
 
 				
@@ -62,10 +52,12 @@ $DB_HOST = "rm-l4v09mz21q0tr948qeo.mysql.me-central-1.rds.aliyuncs.com";
 
 			<div class="wsize1 m-lr-auto">
 				<p class="txt-center l1-txt1 p-b-60">
-					Welcome To  <span class="l1-txt2">eBook Library</span>
+					Welcome To  <span class="l1-txt2">eBook Library!</span><br><br>
+				
+				
+					<span style="font-size: medium;">Enjoy reading digital books, all for free and available anytime and anywhere..</span>
 				</p>
-
-				<form class="w-full flex-w flex-c-m validate-form" action="index.php">
+				<form class="w-full flex-w flex-c-m validate-form" >
 
 					<p class="txt-center s1-txt1 p-t-5">
 						Enter the title of book you wish to read
@@ -93,15 +85,15 @@ $sql="SELECT *
 FROM book
 WHERE title LIKE %$title%";
 
-$result=mysql_query($sql);
-mysql_query('SET CHARACTER SET utf8');
+$result=mysqli_query($sql);
+mysqli_query('SET CHARACTER SET utf8');
 
-while($row = mysql_fetch_array($result)) {
+while($row = mysqli_fetch_array($result)) {
 	echo "
 	<div class='flex-w flex-c-m cd100 wsize1 m-lr-auto p-t-116'>
 	<div class='flex-col-c-m size2 bor1 m-l-10 m-r-10 m-b-15'>
-					<a href='".$row ['file'];."'> <span class='l1-txt3 p-b-9 days'>".$row ['title'];."</span></a>
-					<span class='s1-txt2'>".$row ['author'];."</span>
+					<a href='".$row['file'];."'> <span class='l1-txt3 p-b-9 days'>".$row['title'];."</span></a>
+					<span class='s1-txt2'>".$row['author'];."</span>
 				</div>";
 
 
