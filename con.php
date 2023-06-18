04c16ca625e5cde3597dcccfc19c8096a3ffbc64
Maymoonah  <may.aloulah@gmail.com>
2023-06-18 21:00:23 +0300

    edit end point

--- con.php
+++ con.php
@@ -1,7 +1,7 @@
 <?php 
 // connection to db 
 
-	$DB_HOST = "rm-l4v09mz21q0tr948qeo.mysql.me-central-1.rds.aliyuncs.com";
+	$DB_HOST = "rm-l4vfr6frdw2sgh7flco.mysql.me-central-1.rds.aliyuncs.com";
 	$DB_PORT = "3306";
     $DB_USER = "db_protestacc";
     $DB_PASSWORD = "Amalshamrani1010$$";
@@ -10,20 +10,18 @@
 	
 	
 
-	//Connect to mysql server
+	//Connect to db server
+	$con = new mysqli($DB_HOST,$DB_USER, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
 
-
-	$con = new mysqli($DB_HOST, $DB_PORT,$DB_USER, $DB_PASSWORD,$DB_DATABASE);
-
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
+    if ($con->connect_error()) {
+       die("Connection Failed".$con ->connect_error);}
+       
 
 
 	//if(!$con) {
 	
         //die('Failed to connect to server: ' . mysql_error());
-	}
+	//}
 	
 	//Select database
 	//$db = mysql_select_db($DB_DATABASE);
