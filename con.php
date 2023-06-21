5720da46a5e711778875fb98a01ed12bbf9a80b5
iMaymoonah <133688281+iMaymoonah@users.noreply.github.com>
2023-06-21 20:16:15 +0300

    Update con.php

--- con.php
+++ con.php
@@ -1,22 +1,43 @@
 <?php 
 // connection to db 
 
-	$DB_HOST = "rrm-l4v09mz21q0tr948qeo.mysql.me-central-1.rds.aliyuncs.com";
-	$DB_PORT = "3306";
+$DB_HOST = "rm-l4v9j450xpi4rwhdv7o.mysql.me-central-1.rds.aliyuncs.com";
+    $DB_PORT = "3306";
     $DB_USER = "db_protestacc";
     $DB_PASSWORD = "Amalshamrani1010$$";
     $DB_DATABASE = "db_protest";
-	
+    $table = "book";
 	
 	
 
 	//Connect to db server
-	$con = new mysqli($DB_HOST,$DB_USER, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
-
-    if ($con->connect_error()) {
-       die("Connection Failed".$con ->connect_error);}
+	//$con = new mysqli($DB_HOST,$DB_USER, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
+   // if ($con->connect_error()) {    //   die("Connection Failed".$con ->connect_error);}
        
 
+try {
+  $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_DATABASE", $DB_USER, $DB_PASSWORD);
+  echo "<h2>TODO</h2><ol>"; 
+  foreach($db->query("SELECT * FROM $table") as $row) {
+    echo "<li>" . $row['title'] . "</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+
+
+
+
+
+
+
+
+
+	    
 
 	//if(!$con) {
 	
